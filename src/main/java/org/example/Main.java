bbd8f4c2fdf272a439f9e07fe5d95bd1b693e57a
bhuvaneshwari-k <123533635+bhuvaneshwari-k@users.noreply.github.com>
2023-01-30 16:04:35 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
-
+import java.util.logging.*;
 import java.util.*;
-class creditcard implements Cloneable{
+class creditcard implements cloneable{
     String cardnumber1="1202340456067808";
     String cardholdername;
     String cardnumber;
@@ -25,32 +25,31 @@ class creditcard implements Cloneable{
 
     public static void main(String[] args) throws CloneNotSupportedException{
         Scanner s=new Scanner(System.in);
-        System.out.println("Enter card holder's name:");
+        static Logger l=Logger.getLogger("com.api.jar");
+        l.info("Enter card holder's name:");
         String name=s.nextLine();
-        System.out.println("Enter card number:");
+        l.info("Enter card number:");
         String number=s.nextLine();
-        System.out.println("Enter expiration date:");
+        l.info("Enter expiration date:");
         String date=s.nextLine();
         creditcard cc=new creditcard(name,number,date);
-        System.out.println("The new credit card details are:-");
-        System.out.println("The card holder's name is:"+cc.cardholdername);
-        System.out.println("The card number is:"+cc.cardnumber);
-        System.out.println("The expiration date is:"+cc.expirydate);
-
+        l.info("The new credit card details are:-");
+        l.log(Level.INFO, () -> "The card holder's name is:" +cc.cardholdername);
+        l.log(Level.INFO, () -> "The card number is:" +cc.cardnumber);
+        l.log(Level.INFO, () -> "The expiration date is:" +cc.expirydate);
         String result=cc.checknumber();
         if(result.equals("true")){
-            System.out.println("your card number is correct.");
+            l.info("your card number is correct.");
         }
         else{
-            System.out.println("your card number is not correct.");
+            l.info("your card number is not correct.");
 
         }
         creditcard cc1=(creditcard)cc.clone();
-        System.out.println("The cloned credit card details are:-");
-        System.out.println("The card holder's name is:"+cc1.cardholdername);
-        System.out.println("The card number is:"+cc1.cardnumber);
-        System.out.println("The expiration date is:"+cc1.expirydate);
-
+        l.info("The cloned credit card details are:-");
+         l.log(Level.INFO, () -> "The card holder's name is:" +cc1.cardholdername);
+         l.log(Level.INFO, () -> "The card number is:" +cc1.cardnumber);
+         l.log(Level.INFO, () -> "The expiration date is:" +cc1.expirydate);
     }
 
 }
